904d53239c4fea422714d09cbf326e1c9a16a3f3
VaishaliVetrivendhan <36623866+VaishaliVetrivendhan@users.noreply.github.com>
2018-02-23 22:33:59 +0530

    Update Hello.java

--- Beginner Level/Hello.java
+++ Beginner Level/Hello.java
@@ -2,11 +2,18 @@ import java.util.*;
 class Hello{
 public static void main(String args[]){
 int n=0;
+try{
 Scanner s=new Scanner(System.in);
 n=s.nextInt();
 for(int i=1;i<=n;i++)
 {
 System.out.println("Hello");
+}
+}
+catch(Exception e)
+{
+ System.out.println("You entered invalid data");
+  System.out.println("Run the program again");
 }
 }
 }
