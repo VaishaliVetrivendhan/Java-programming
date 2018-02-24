8b537ee2132574804fbbed524089753cf28f03aa
VaishaliVetrivendhan <36623866+VaishaliVetrivendhan@users.noreply.github.com>
2018-02-24 20:35:40 +0530

    Create Large.java

---
+++ Beginner Level/Large.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class Large{
+public static void main(String args[]){
+int a=0,b=0,c=0;
+try{
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+b=s.nextInt();
+c=s.nextInt();
+if(a>b&&a>c){
+System.out.println(a);
+}
+else if(b>a&&b>c){
+System.out.println(b);
+}
+else{
+System.out.println(c);
+}
+}
+catch(Exception e){
+System.out.println("You entered invalid data");
+System.out.println("Run the program again");
+}
+}
+}
