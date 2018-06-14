25b95353a436efbd83852fcea12255f23e632fe2
VaishaliVetrivendhan <36623866+VaishaliVetrivendhan@users.noreply.github.com>
2018-06-14 20:30:38 +0530

    Create Eveninterval.java

---
+++ Beginner Level/Eveninterval.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Eveninterval{
+public static void main(String args[]){
+int start=0,end=0;
+try{
+Scanner sc=new Scanner(System.in);
+start=sc.nextInt();
+end=sc.nextInt();
+for(int i=start;i<=end;i++)
+{
+if(i%2==0)
+System.out.print(i+" ");
+}
+}
+catch(Exception e){
+System.out.println("You entered invalid data"+"\n"+"Run the program again");
+}
+}
+}
