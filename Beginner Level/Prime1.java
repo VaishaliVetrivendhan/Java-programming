e71c840fa8f5f0a0c59ce22a24aec4de278e48d1
VaishaliVetrivendhan <36623866+VaishaliVetrivendhan@users.noreply.github.com>
2018-06-19 23:07:36 +0530

    Create Prime1.java

---
+++ Beginner Level/Prime1.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Prime{
+public static void main(String args[]){
+try{
+int n,flag=0;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+for(int i=0;i<n/2;i++)
+{
+if(n%2==0){
+flag=1;
+break;
+}
+}
+if(flag==0){
+System.out.println("yes");
+}
+else{
+System.out.println("no");
+}
+}
+catch(Exception e){
+System.out.println("Invalid data.\nRun again");
+}
+}
+}
